84e8f4d9f3c4088f564e35b0b6ec7a9f47d6b48b
chivorotkiv <chivorotkiv@omich.net>
2015-12-07 20:47:15 +0600

    [3] - Build tests for running in browser.

--- test/test-browser.js
+++ test/test-browser.js
@@ -2,7 +2,7 @@
 var nodeunit = require('nodeunit')
 var r = require('nodeunit/lib/reporters/browser.js')
 window.nodeunit = nodeunit
-require('../src/**/*.js',  {mode: 'expand'})
 var files = require('./tests/**/*-test.js',  {mode: 'hash'})
+console.log('Test files:', files)
 nodeunit.reporters.browser = r
 nodeunit.reporters.browser.run(files)
--- test/test-lib.js
+++ test/test-lib.js
@@ -1,9 +1,13 @@
 'use strict'
 try {
-    require('../lib/**/*.js',  {mode: 'expand'})
+  require('./**/!(*-test).js',  {mode: 'expand'})
 } catch (e) {
+  if (typeof window !== 'undefined') {
+    throw e
+  } else {
     // Do nothing. Looks like we just run in normal mode instead of browserify
+  }
 }
 exports.require = function(path) {
-    return require('./' + path)
+  return require('./' + path)
 }
